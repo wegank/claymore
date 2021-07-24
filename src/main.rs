c5b9de40059ca1cc7c0a4ead0fcdf5c52860fa71
Weijia Wang <9713184+wegank@users.noreply.github.com>
2021-07-24 14:35:54 +0200

    Rename util to keyinfo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod util;
+mod keyinfo;
 
 fn main() {
     let key = String::from("BCDFG-HJKNM-PQRTV-WXY23-46789");
-    let key_info = util::KeyInfo::load(&key)
+    let key_info = keyinfo::KeyInfo::load(&key)
         .unwrap_or_else(|error| panic!("{}", error));
     println!("{:#?}", key_info);
     println!("{}", key_info.save());
