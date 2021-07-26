e5230f707933666aaeae4efed4afab71ed4a63dd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2021-07-26 13:30:10 +0200

    Update example

--- examples/keygen.rs
+++ examples/keygen.rs
@@ -1,16 +1,20 @@
 use claymore::KeyInfo;
-use rand::Rng;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let key_info = KeyInfo {
+    let mut key_info = KeyInfo {
         group_id: 1785,
-        serial_number: rng.gen_range(0..=53501099),
-        fst_security_value: rng.gen_range(0..=0xffffffffff),
-        snd_security_value: rng.gen_range(0..=0x1fff),
+        serial_number: 0,
+        fst_security_value: 0,
+        snd_security_value: 0,
         checksum: 0,
         upgrade_bit: 0
     };
-    let key = key_info.hash().save();
-    println!("{}", key);
+    println!("{}", keygen(&mut key_info));
+}
+
+fn keygen(key_info: &mut KeyInfo) -> String {
+    key_info.serial_number = rand::random::<u32>() % 53501100;
+    key_info.fst_security_value = rand::random::<u64>() & 0xffffffffff;
+    key_info.snd_security_value = rand::random::<u32>() & 0x1fff;
+    key_info.hash().save()
 }
