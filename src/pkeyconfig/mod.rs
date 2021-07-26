ce7f0095a162a3ce63f52d9a80f2c219233476fa
Weijia Wang <9713184+wegank@users.noreply.github.com>
2021-07-26 13:21:47 +0200

    Add a PID generator

--- src/pkeyconfig/mod.rs
+++ src/pkeyconfig/mod.rs
@@ -1,5 +1,6 @@
 mod decode;
 use decode::{Configuration, KeyRange, PublicKey};
+use chrono::prelude::*;
 
 const PKEY_INVALID: &str = "Invalid product key.";
 
@@ -12,6 +13,8 @@ pub struct PKeyConfig {
 
 #[derive(Debug)]
 pub struct PKeyConfigInfo {
+    pub product_id: String,
+    pub extended_pid: String,
     pub act_config_id: String,
     pub edition_id: String,
     pub product_description: String,
@@ -37,7 +40,8 @@ impl PKeyConfig {
         }
     }
 
-    pub fn query(&self, group_id: u32, serial_number: u32) -> Result<PKeyConfigInfo, String> {
+    pub fn query(&self, group_id: u32, serial_number: u32, upgrade_bit: u8)
+        -> Result<PKeyConfigInfo, String> {
         let configuration = match self.configurations.iter()
             .filter(|&config| config.ref_group_id == group_id)
             .collect::<Vec<_>>().get(0) {
@@ -62,13 +66,36 @@ impl PKeyConfig {
             Some(_) => (),
             _ => return Err(PKEY_INVALID.to_string()),
         };
-        let act_config_id = configuration.act_config_id.clone();
+        let mut product_id = format!("{:06}", group_id)
+            + &format!("{:09}", serial_number) + "AA"
+            + &format!("{:03}", rand::random::<u16>() % 1000);
+        for i in (5..23).step_by(6) {
+            product_id.insert(i, '-');
+        }
+        let mut extended_pid = "XXXXX-".to_string()
+            + &format!("{:05}", group_id)
+            + &format!("{:09}", serial_number) + "-"
+            + &format!("{:02}",
+                       match key_range.eula_type.as_str() {
+                           "OEM" => 2,
+                           "Volume" => 3,
+                           _ => upgrade_bit,
+                       }) + "-1033-9200.0000-"
+            + &format!("{:03}", Utc::today().ordinal())
+            + &format!("{:04}", Utc::today().year());
+        for i in (11..16).step_by(4) {
+            extended_pid.insert(i, '-');
+        }
+        let mut act_config_id = configuration.act_config_id.clone();
+        act_config_id = act_config_id[1..act_config_id.len()-1].to_string();
         let edition_id = configuration.edition_id.clone();
         let product_description = configuration.product_description.clone();
         let product_key_type = configuration.product_key_type.clone();
         let part_number = key_range.part_number.clone();
         let eula_type = key_range.eula_type.clone();
         Ok(PKeyConfigInfo {
+            product_id,
+            extended_pid,
             act_config_id,
             edition_id,
             product_description,
@@ -79,6 +106,6 @@ impl PKeyConfig {
     }
 
     pub fn is_valid(&self, group_id: u32, serial_number: u32) -> bool {
-        self.query(group_id, serial_number).is_ok()
+        self.query(group_id, serial_number, 0).is_ok()
     }
 }
