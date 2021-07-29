6e654d9cd1e8488748c656705105d6a485940a72
Weijia Wang <9713184+wegank@users.noreply.github.com>
2021-07-29 14:10:56 +0200

    Update example

--- examples/pid_checker.rs
+++ examples/pid_checker.rs
@@ -1,17 +1,20 @@
-use claymore::{ProductInfo, LicenseInfo};
-use claymore::ProductConfigUsingKeys;
+use claymore::{ProductInfo, ProductConfigUsingKeys};
+use claymore::{LicenseInfo, LicenseInfoUsingKeys};
 
 const KEY: &str = "RR3BN-3YY9P-9D7FC-7J4YF-QGJXW";
 const TEXT: &str = include_str!("../resources/pkeyconfig-win8.xrm-ms");
 
 fn main() {
     let product_config = ProductInfo::load(TEXT).unwrap();
-    assert!(product_config.is_valid_key(KEY));
-
-    let pkey_info = product_config.query_key(KEY).unwrap();
-    println!("{:#?}", pkey_info);
-
-    let pid = &pkey_info.extended_pid;
-    let license_info = LicenseInfo::load_one(pid).unwrap();
-    assert!(!license_info.is_valid(pid));
+    product_config.print_key_info(KEY);
+    if !product_config.is_valid_key(KEY) {
+        return;
+    }
+    let product_info = product_config.query_key(KEY).unwrap();
+    if !product_info.product_key_type.contains("Volume") {
+        return;
+    }
+    let pid = product_config.query_key(KEY).unwrap().extended_pid;
+    let license_info = LicenseInfo::load_one(&pid).unwrap();
+    license_info.print_key_info(&pid);
 }
