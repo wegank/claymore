cda3bd97bd6afce9d577c6fbc23e9d08a44d650c
Weijia Wang <9713184+wegank@users.noreply.github.com>
2021-07-29 13:13:52 +0200

    Include pkeyconfig at compile time

--- examples/pid_checker.rs
+++ examples/pid_checker.rs
@@ -2,10 +2,10 @@ use claymore::{ProductInfo, LicenseInfo};
 use claymore::ProductConfigUsingKeys;
 
 const KEY: &str = "RR3BN-3YY9P-9D7FC-7J4YF-QGJXW";
-const PATH: &str = "resources/pkeyconfig-win8.xrm-ms";
+const TEXT: &str = include_str!("../resources/pkeyconfig-win8.xrm-ms");
 
 fn main() {
-    let product_config = ProductInfo::load_from_file(PATH).unwrap();
+    let product_config = ProductInfo::load(TEXT).unwrap();
     assert!(product_config.is_valid_key(KEY));
 
     let pkey_info = product_config.query_key(KEY).unwrap();
